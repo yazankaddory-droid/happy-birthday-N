b39bf82a6cec2391278337af69cc53d4db282669
yazankaddory-droid <yazan.kaddory2@gmail.com>
2026-08-25 02:26:03 +0200

    Fix typo in placeholder message

--- js/config.js
+++ js/config.js
@@ -12,7 +12,7 @@
 const SITE_CONFIG = {
   videoEnabled: false,
   videoFile: "",
-  placeholderMessage: `due to a problem on my site, i couldn't add a video :( but hopefully next time i can :)`,
+  placeholderMessage: `due to a problem on my side, i couldn't add a video :( but hopefully next time i can :)`,
   boxLabel: "Birthday of 2026 To Nyt",
   message: `Haiiii. Happy birthday Nyt! welcome to being 17 years old! You are an amazing friend and i wish the best for you no matter what.
 
